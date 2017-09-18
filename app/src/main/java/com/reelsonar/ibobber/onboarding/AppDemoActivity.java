32b516dfc5c0496e686c362e20e609b09c19e48a
Rujul <imobdevdevice@gmail.com>
2017-09-18 11:54:11 +0530

    data binding on app demo activity

--- app/src/main/java/com/reelsonar/ibobber/onboarding/AppDemoActivity.java
+++ app/src/main/java/com/reelsonar/ibobber/onboarding/AppDemoActivity.java
@@ -13,16 +13,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.RelativeLayout;
 
 import com.reelsonar.ibobber.BobberApp;
 import com.reelsonar.ibobber.R;
+import com.reelsonar.ibobber.databinding.FragmentAppDemoBinding;
 import com.reelsonar.ibobber.model.Intro;
 import com.reelsonar.ibobber.settings.ScreenSlidePageFragment;
 import com.reelsonar.ibobber.util.Actions;
@@ -39,21 +36,11 @@ public class AppDemoActivity extends AppCompatActivity {
     public final static int INITIAL_DEMO_IS_TRUE = 1;
     private boolean mIsInitialDemo = false;
 
-    static private View mFragmentView;
-
-    //    static private ViewFlipper mViewFlipper;
-    static private int mCurDemoView;
-
-    static private GestureDetector mDetector;
-    private ViewPager mPager;
-    static private Animation mSlideLeftIn;
-    static private Animation mSlideLeftOut;
-    static private Animation mSlideRightIn;
-    static private Animation mSlideRightOut;
 
     static private PageIndicator mPageIndicator;
     private ScreenSlidePagerAdapter mPagerAdapter;
     private ArrayList<Intro> introArray;
+    private FragmentAppDemoBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -73,12 +60,8 @@ public class AppDemoActivity extends AppCompatActivity {
             }
         }
 
-        mSlideLeftIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
-        mSlideLeftOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_left);
-        mSlideRightIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
-        mSlideRightOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_right);
 
-        mCurDemoView = 0;
+
     }
 
 
@@ -94,49 +77,11 @@ public class AppDemoActivity extends AppCompatActivity {
     }
 
     public void onNextButton(View v) {
-        mPager.setCurrentItem(mPager.getCurrentItem() + 1);
-//        if (mViewFlipper != null && mCurDemoView < NUMBER_OF_DEMO_VIEWS - 1) {
-//
-//            mCurDemoView++;
-//            mPageIndicator.mCurrentPage = mCurDemoView;
-//            mPageIndicator.invalidate();
-//
-//            mViewFlipper.setOutAnimation(mSlideLeftOut);
-//            mViewFlipper.setInAnimation(mSlideRightIn);
-//            mViewFlipper.showNext();
-//
-//            //done button for last page
-//            if (mCurDemoView == NUMBER_OF_DEMO_VIEWS - 1) {
-//                Button nextButton = (Button) mFragmentView.findViewById(R.id.nextButton);
-//                nextButton.setVisibility(View.GONE);
-//                Button doneButton = (Button) mFragmentView.findViewById(R.id.doneButton);
-//                doneButton.setVisibility(View.VISIBLE);
-//            }
-//        }
-
+        binding.viewPager.setCurrentItem(binding.viewPager.getCurrentItem() + 1);
     }
 
     public void onPrevButton(View v) {
-        mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-//        if (mViewFlipper != null) {
-//
-//            if (mCurDemoView > 0) {
-//                if (mCurDemoView == NUMBER_OF_DEMO_VIEWS - 1) {
-//                    Button nextButton = (Button) mFragmentView.findViewById(R.id.nextButton);
-//                    nextButton.setVisibility(View.VISIBLE);
-//                    Button doneButton = (Button) mFragmentView.findViewById(R.id.doneButton);
-//                    doneButton.setVisibility(View.GONE);
-//                }
-//                mViewFlipper.setOutAnimation(mSlideRightOut);
-//                mViewFlipper.setInAnimation(mSlideLeftIn);
-//                mViewFlipper.showPrevious();
-//                mCurDemoView--;
-//                mPageIndicator.mCurrentPage = mCurDemoView;
-//                mPageIndicator.invalidate();
-//
-//            }
-//        }
-
+        binding.viewPager.setCurrentItem(binding.viewPager.getCurrentItem() - 1);
     }
 
     public class AppDemoFragment extends Fragment {
@@ -151,20 +96,15 @@ public class AppDemoActivity extends AppCompatActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_app_demo, container, false);
-            mFragmentView = rootView;
+            binding = FragmentAppDemoBinding.inflate(getLayoutInflater());
 
-            RelativeLayout relativeLayout = (RelativeLayout) mFragmentView.findViewById(R.id.pageIndicatorLayout);
             mPageIndicator = new PageIndicator(getActivity());
-//            mPageIndicator.mTotalPages = NUMBER_OF_DEMO_VIEWS;
             mPageIndicator.mCurrentPage = 0;
+            binding.pageIndicatorLayout.addView(mPageIndicator);
 
-            relativeLayout.addView(mPageIndicator);
-
-            mPager = (ViewPager) mFragmentView.findViewById(R.id.view_pager);
             mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-            mPager.setAdapter(mPagerAdapter);
-            mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            binding.viewPager.setAdapter(mPagerAdapter);
+            binding.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
                 @Override
                 public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -181,7 +121,7 @@ public class AppDemoActivity extends AppCompatActivity {
 
                 }
             });
-            return rootView;
+            return binding.getRoot();
         }
 
     }
@@ -228,7 +168,7 @@ public class AppDemoActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 13;
+            return NUMBER_OF_DEMO_VIEWS;
         }
     }
 }
