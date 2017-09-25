c6165348031740112ae02aee11b346637a0f11bc
Rujul <imobdevdevice@gmail.com>
2017-09-25 16:14:03 +0530

    after added hebrew and TIẾNG VIỆT

--- app/src/main/java/com/reelsonar/ibobber/util/CallBack.java
+++ app/src/main/java/com/reelsonar/ibobber/util/CallBack.java
@@ -4,18 +4,14 @@ import retrofit2.Call;
 import retrofit2.Response;
 
 /**
- * Created by Manoj Singh
+ * Created by rujul on 22/9/17.
  */
 
-public interface CallBack {
-
-//    <T> void onResponse(Call call, Response response, String msg, Class<T> clazz);
-
-    void onResponse(Call call, Response response, String msg);
+public interface GenericCallBack {
+    <T> void onResponse(Call call, Response response, String msg, Object object);
 
     void onFail(Call call, Throwable e);
 
     void onSocketTimeout(Call call, Throwable e);
-
 
 }
---
+++ app/src/main/java/com/reelsonar/ibobber/util/CallBackOld.java
@@ -0,0 +1,21 @@
+package com.reelsonar.ibobber.util;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+/**
+ * Created by Manoj Singh
+ */
+
+public interface CallBack {
+
+//    <T> void onResponse(Call call, Response response, String msg, Class<T> clazz);
+
+    void onResponse(Call call, Response response, String msg);
+
+    void onFail(Call call, Throwable e);
+
+    void onSocketTimeout(Call call, Throwable e);
+
+
+}
